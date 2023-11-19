06fa995c359cea4b659a139a5e20f5f2b5e383a2
Sasha Boulogne <sboulogn@k0r4p9.42mulhouse.fr>
2023-11-19 16:57:17 +0100

    cpp08

--- CPP08/ex02/MutanStack.hpp
+++ CPP08/ex02/MutanStack.hpp
@@ -3,17 +3,14 @@
 #include <iostream>
 #include <stack>
 #include <deque>
+#include <list>
 
 template <typename T, typename Container = std::deque<T>>
 class MutantStack : public std::stack<T, Container> {
 public:
-    // Explicitly define iterator and const_iterator
     typedef typename Container::iterator iterator;
     typedef typename Container::const_iterator const_iterator;
 
-    // ... (other member functions from std::stack)
-
-    // Iterator definitions
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end(); }
 
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -18,11 +18,37 @@ int main(void) {
 
     std::cout << "Iterating through MutantStack:" << std::endl;
     while (it != ite) {
-        std::cout << "i" << *it << std::endl;
+        std::cout << *it << std::endl;
         ++it;
     }
 
     std::stack<int> s(mstack); // Can still be used as a regular std::stack
+
+    std::cout << "============================================" << std::endl;
+
+    std::list<int> l;
+    l.push_back(5);
+    l.push_back(17);
+    std::cout << "Top of stack: " << l.front() << std::endl;
+
+    l.pop_back();
+    std::cout << "Size after pop: " << l.size() << std::endl;
+
+    l.push_back(3);
+    l.push_back(5);
+    l.push_back(737);
+
+    std::list<int>::iterator it2 = l.begin();
+    std::list<int>::iterator ite2 = l.end();
+
+    std::cout << "Iterating through MutantStack:" << std::endl;
+    while (it2 != ite2) {
+        std::cout << *it2 << std::endl;
+        ++it2;
+    }
+
+    std::list<int> u(l); // Can still be used as a regular std::stack
+
 
     return 0;
 }
